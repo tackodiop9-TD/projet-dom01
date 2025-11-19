6d49d7ca5659f4ad9222dcff44e118a5f36a0551
tackodiop9-TD <tackodiop9@gmail.com>
2025-11-19 15:14:40 +0000

    mise a jour

--- script.js
+++ script.js
@@ -1,71 +1,126 @@
-// Get Buttons
-let PlusButton = document.querySelectorAll('.btnPlus');
+// class ShoppingCart {
+//   constructor() {
+//     this.plusButton = document.querySelectorAll('.btnPlus');
+//     this.deleteButton = document.querySelectorAll('.btn-outline-danger');
+//     this.heartIcons = document.querySelectorAll('.fa-heart');
+//     this.article = document.querySelectorAll('.article');
+//     this.qty = document.querySelectorAll('.quantite');
+//     this.total = document.querySelectorAll('.total');
+//     this.price = document.querySelectorAll('.prix');
+//     this.mns = document.querySelectorAll('.btn-outline-warning');
+//     this.sum = document.querySelector('.Prixtotal');
+//     this.init();
+
+   
+
+//   }
+
+//   calculateTotal() {
+//     let totalPrice = 0;
+//     for (let i = 0; i < this.article.length; i++) {
+//       let itemPrice = parseInt(this.price[i].innerHTML);
+//       let itemQty = parseInt(this.qty[i].value);
+//       let itemTotal = itemPrice * itemQty;
+//       this.total[i].innerHTML = itemTotal;
+//       totalPrice += itemTotal;
+//     }
+//     this.sum.value = totalPrice;
+//   }
+
+//   init() {
+//     for (let i = 0; i < this.plusButton.length; i++) {
+//       this.plusButton[i].addEventListener('click', () => {
+//         this.qty[i].value++;
+//         this.calculateTotal();
+//       });
+
+//       this.mns[i].addEventListener('click', () => {
+//         if (this.qty[i].value > 0) {
+//           this.qty[i].value--;
+//           this.calculateTotal();
+//         }
+//       });
+
+//       this.deleteButton[i].addEventListener('click', () => {
+//         this.article[i].remove();
+//         let articlePrice = parseInt(this.price[i].innerHTML) * parseInt(this.qty[i].innerHTML);
+//         let currentTotal = parseInt(this.sum.value);
+//         this.sum.value = currentTotal - articlePrice;
+//         this.calculateTotal();
+//       });
+//     }
+
+//     this.heartIcons.forEach((icon) => {
+//       icon.addEventListener('click', () => {
+//         icon.classList.toggle('liked');
+//       });
+//     });
+
+//     this.calculateTotal();
+//   }
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   new ShoppingCart();
+// });
+
+//         let articlePrice = parseInt(this.price[i].innerHTML) * parseInt(this.qty[i].value);
+
+
+
+// --------------------get all the buttons--------------------------------
+var plusButton = document.querySelectorAll('.btnPlus');
 let deleteButton = document.querySelectorAll('.btn-outline-danger');
 let heartIcons = document.querySelectorAll('.fa-heart');
-let article = document.querySelectorAll('.article');
-let qty = document.querySelectorAll('.quantite');
-let total = document.querySelectorAll('.total');
-let price = document.querySelectorAll('.prix');
-let mns = document.querySelectorAll('.btn-outline-warning');
-let sum = document.querySelector('.Prixtotal');
-
-
-// Calculate total of all
-function CalculateTotal() {
-    let totalPrice = 0;
-
-    for (let i = 0; i < article.length; i++) {
-
-        let itemPrice = parseInt(price[i].innerHTML);
-        let itemQty = parseInt(qty[i].value);
-
-        let itemTotal = itemPrice * itemQty;   // CORRECTION ICI !
-
-        total[i].innerHTML = itemTotal;
-        totalPrice += itemTotal;
+let article = document.querySelectorAll('.article')
+let qty = document.querySelectorAll('.quantite')
+let total = document.querySelectorAll('.total')
+let price = document.querySelectorAll('.prix')
+let mns = document.querySelectorAll('.btn-outline-warning')
+let sum = document.querySelector('.Prixtotal')
+// -----------------calculate the total price of the products---------------
+function calculateTotal() {
+  let totalPrice = 0;
+  for (let i = 0; i < article.length; i++) {
+    let itemPrice = parseInt(price[i].innerHTML);
+    let itemQty = parseInt(qty[i].value);
+    let itemTotal = itemPrice * itemQty;
+    total[i].innerHTML = itemTotal;
+    totalPrice += itemTotal;
+  }
+  sum.value = totalPrice;
+  console.log(article);
+}
+// ----------------add a click event listener to each plus button----------
+for (let i = 0; i < plusButton.length; i++) { 
+  plusButton[i].addEventListener('click', function(){
+    qty[i].value ++ ;
+    calculateTotal();
+  });
+  // -------------add a click event listener to each minus button-------------
+  mns[i].addEventListener('click' , function(){
+    if(qty[i].value > 1){
+      qty[i].value --;
+      calculateTotal();
     }
-
-    
-        sum.value = totalPrice;
-    
+  })
+  // -------------add a click event listener to each delete button------------
+  deleteButton[i].addEventListener('click' , function(){
+    article[i].remove();
+    let articlePrice = parseInt(price[i].innerHTML) * parseInt(qty[i].innerHTML);
+    let currentTotal = parseInt(sum[0].value);
+    sum[0].value = currentTotal - articlePrice;
+    calculateTotal()
+  })
 }
-
-
-// Plus Button
-for (let i = 0; i < PlusButton.length; i++) {
-    PlusButton[i].addEventListener('click', function () {
-        qty[i].value++;
-        CalculateTotal();
-    });
-}
-
-
-// Minus Button
-for (let i = 0; i < mns.length; i++) {
-    mns[i].addEventListener('click', function () {
-        if (qty[i].value > 1) {
-            qty[i].value--;
-            CalculateTotal();
-        }
-    });
-}
-
-
-// Delete Button
-for (let i = 0; i < deleteButton.length; i++) {
-    deleteButton[i].addEventListener('click', function () {
-
-        article[i].remove();
-        CalculateTotal();
-    });
-}
-
-
-// Heart Icon
+// -----------add a click event listener to each heart icon------------
 heartIcons.forEach((icon) => {
-    icon.addEventListener('click', function () {
-        icon.classList.toggle('liked'); 
-    });
+  icon.addEventListener('click', () => {
+    // toggle the 'liked' class on the clicked icon
+    icon.classList.toggle('liked');
+  });
 });
+// -------------------calculate the initial total price on page load------------------
+calculateTotal(); 
 
-CalculateTotal();
+
